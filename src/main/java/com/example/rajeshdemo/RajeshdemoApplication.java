f3e12c8320809a13a2a2f021d8d22ba2560d72a5
RAJESH <rajeshsmart.rs21@gmail.com>
2024-07-19 13:29:14 +0530

    first

---
+++ src/main/java/com/example/rajeshdemo/RajeshdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rajeshdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajeshdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajeshdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rajeshdemo/RajeshdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rajeshdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajeshdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
